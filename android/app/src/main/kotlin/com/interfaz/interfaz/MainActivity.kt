b6db4ac23ca50dfe0baa6e13666b46a9d92c61da
Aljandro Sierra <alejosierrarodriguez2@gmail.com>
2022-11-02 19:42:43 -0500

    Seguimiento Sprint 2, desarrollo móvil

---
+++ android/app/src/main/kotlin/com/interfaz/interfaz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.interfaz.interfaz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/logine_page.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:interfaz/models/user.dart';
+import 'package:interfaz/pages/home_page.dart';
+import 'package:interfaz/pages/register_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  User userLoad = User.Empty();
+
+  @override
+  void initState(){
+    _getUser();
+    super.initState();
+  }
+
+  _getUser() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Map<String, dynamic> userMap = jsonDecode(prefs.getString("user")!);
+    userLoad = User.fromJson(userMap);
+}
+
+  void _showMsg(String msg){
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+        SnackBar(content: Text(msg),
+          action: SnackBarAction(
+              label: 'Aceptar', onPressed: scaffold.hideCurrentSnackBar),
+        )
+    );
+  }
+
+  void _validateUser(){
+    if(_email.text == userLoad.email && _password.text == userLoad.password){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }else {
+      _showMsg("Correo o contraseña incorrecta!");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(image: AssetImage('assets/images/logo.png'),),
+              const SizedBox(height: 16.0,),
+              TextFormField(
+                controller: _email,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),labelText: 'Correo electrónico'
+                  ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+
+              const SizedBox(height: 16.0,),
+              TextFormField(
+                controller: _password,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),labelText: 'Contraseña'
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 16.0,),
+              
+              ElevatedButton(
+                  onPressed: () {
+                    _validateUser();
+              }, child: Text ('Iniciar Sesión')),
+
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.blue)
+                  ),
+                    onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterPage()));
+                  },
+                child: const Text('Registrarse'),
+
+                ),
+
+            ],
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+}
